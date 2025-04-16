ecbfd1a07c7b991e054d9fb9e2725f85d6fc1d7b
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2025-04-16 18:36:37 -0300

    commit inicial

---
+++ pi.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <omp.h>
+
+static long num_steps = 100000;
+#define MAX 100
+
+int main() {
+    double step = 1.0/(double)num_steps;
+    
+    double sum[MAX] = {0.0};
+    #pragma omp parallel num_threads(MAX)
+    {
+        int ID = omp_get_thread_num();
+        for (int i = 0; i < (num_steps / MAX); i++) {
+            double x = ((ID * num_steps / MAX) + i + 0.5) * step;
+            sum[ID] += 4.0 / (1.0 + x*x);
+        }
+    }
+    double sum_total = 0.0;
+    for (int i = 0; i < MAX; i++) {
+        sum_total += sum[i];
+    }
+
+    double pi = step * sum_total;
+
+    printf("pi = %.15lf\n", pi);
+
+    return 0;
+}
